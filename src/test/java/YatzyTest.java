37cd092d065403b6abeebde264a5b428a1e58a6f
fruizotero <frank.git96@gmail.com>
2023-11-30 18:06:19 +0100

    test(YatzyTest): Modificación de valor esperado en el test largeStraight

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -101,9 +101,9 @@ public class YatzyTest {
 
     @Test
     public void largeStraight() {
-        assertEquals(20, Yatzy.largeStraight(6, 2, 3, 4, 5));
-        assertEquals(20, Yatzy.largeStraight(2, 3, 4, 5, 6));
-        assertEquals(0, Yatzy.largeStraight(1, 2, 2, 4, 5));
+        assertEquals(0, Yatzy.largeStraight(new int[]{6, 2, 3, 4, 5}));
+        assertEquals(20, Yatzy.largeStraight(new int[]{2, 3, 4, 5, 6}));
+        assertEquals(0, Yatzy.largeStraight(new int[]{1, 2, 2, 4, 5}));
     }
 
     @Test
