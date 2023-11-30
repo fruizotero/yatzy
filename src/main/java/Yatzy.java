df31f1dfa02108c38cc56859881d644afb5068eb
fruizotero <frank.git96@gmail.com>
2023-11-30 16:59:46 +0100

    refactor(Yatzy) Modficación de la lógica de los métodos 'kind' y creación de una función común para su calculo

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -82,63 +82,47 @@ public class Yatzy {
 
     }
 
-    public static int score_pair(int[] dices) {
-
+    public static int scoreValueOfAKind(int[] dices, int amount) {
         HashMap<Integer, Integer> counterMatchingDices = counterMatchingValuesDices(dices);
 
         for (int valueDice = 6; valueDice >= 1; valueDice--) {
             int valueMatchingDice = counterMatchingDices.get(valueDice);
-            if (valueMatchingDice >= 2) return valueDice * 2;
+            if (valueMatchingDice >= amount) return valueDice * amount;
         }
         return 0;
     }
 
-    public static int two_pair(int d1, int d2, int d3, int d4, int d5) {
-        int[] counts = new int[6];
-        counts[d1 - 1]++;
-        counts[d2 - 1]++;
-        counts[d3 - 1]++;
-        counts[d4 - 1]++;
-        counts[d5 - 1]++;
-        int n = 0;
+    public static int score_pair(int[] dices) {
+
+        return scoreValueOfAKind(dices, 2);
+    }
+
+    public static int two_pair(int[] dices) {
+
+        int pairs = 0;
         int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6 - i - 1] >= 2) {
-                n++;
-                score += (6 - i);
+        HashMap<Integer, Integer> counterMatchingDices = counterMatchingValuesDices(dices);
+
+        for (int valueDice = 6; valueDice >= 1; valueDice--) {
+            int valueMatchingDice = counterMatchingDices.get(valueDice);
+            if (valueMatchingDice >= 2) {
+                pairs++;
+                score += valueDice;
             }
-        if (n == 2)
-            return score * 2;
-        else
-            return 0;
+
+        }
+        return pairs == 2 ? score * 2 : 0;
+
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1 - 1]++;
-        tallies[_2 - 1]++;
-        tallies[d3 - 1]++;
-        tallies[d4 - 1]++;
-        tallies[d5 - 1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i + 1) * 4;
-        return 0;
+    public static int four_of_a_kind(int[] dices) {
+
+        return scoreValueOfAKind(dices,4);
     }
 
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
-        int[] t;
-        t = new int[6];
-        t[d1 - 1]++;
-        t[d2 - 1]++;
-        t[d3 - 1]++;
-        t[d4 - 1]++;
-        t[d5 - 1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i + 1) * 3;
-        return 0;
+    public static int three_of_a_kind(int[] dices) {
+
+        return scoreValueOfAKind(dices,3);
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
